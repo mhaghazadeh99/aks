d7f77c3975a7af286b9d4e3a9dbcd7795d657e44
mhaghazadeh99 <mhaghazadeh99@gmail.com>
2026-07-13 10:22:46 +0000

    lang

--- IRWA/settings.py
+++ IRWA/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -153,13 +155,29 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "fa"
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LANGUAGES = [
+
+    ("fa", _("Persian")),
+
+    ("en", _("English")),
+
+]
+
+LOCALE_PATHS = [
+
+    BASE_DIR / "locale",
+
+]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Static files (CSS, JavaScript, Images)
--- IRWA/urls.py
+++ IRWA/urls.py
@@ -19,10 +19,17 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+
+    path("i18n/", include("django.conf.urls.i18n")),
+
+]
+urlpatterns += i18n_patterns(
+    
     path('admin/', admin.site.urls),
+    
     path('', include('dashboard.urls')),
     path("contract/", include("contract.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
@@ -44,7 +51,11 @@ urlpatterns = [
         include('operations.urls')
     ),
     
-]
+)
+
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )

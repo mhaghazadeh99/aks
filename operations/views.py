21c7aade72316799822b839152986a4885c8246e
mhaghazadeh99 <mhaghazadeh99@gmail.com>
2026-08-16 08:17:36 +0000

    trans

--- operations/views.py
+++ operations/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 from django.db import transaction
 from django.db.models import Q
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from django.utils import timezone
 import json
 import os
